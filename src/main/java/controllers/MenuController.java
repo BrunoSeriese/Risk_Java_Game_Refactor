bf0fde20ef3aebce159dc0b1b4ad8e01429ff1e2
BrunoSeriese <bruno.pleysier@gmail.com>
2021-06-03 23:11:55 +0200

    De buttons worden nu via controller bestuurd check of de terminal "CHECK" print wanneer er op een knop geklikt wordt

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -1,4 +1,23 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MenuController {
+
+    @FXML
+    private Button endGame;
+    @FXML
+    private Button startGame;
+
+
+    @FXML
+    void handleClick(ActionEvent event){
+        Button sourceButton = (Button) event.getSource();
+
+       System.out.println("CHECK");
+
+    }
 }
