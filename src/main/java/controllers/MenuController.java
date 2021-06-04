f79680c6e802583f7016b664013817360a68e85b
BrunoSeriese <bruno.pleysier@gmail.com>
2021-06-04 17:15:09 +0200

    eerste stage

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -2,8 +2,15 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import views.LobbyView;
+
+import java.io.IOException;
 
 public class MenuController {
 
@@ -14,11 +21,16 @@ public class MenuController {
 
 
     @FXML
-    void handleClick(ActionEvent event){
+    void handleClick(ActionEvent event) throws IOException {
         Button sourceButton = (Button) event.getSource();
 
         if (sourceButton.getId().equals("playGame")){
-            System.out.println("volgende Scherm");
+
+//            LobbyView lobby = new LobbyView();
+
+
+
+
         }
 
        System.out.println(sourceButton.getId());
