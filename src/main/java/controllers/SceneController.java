5e5d67ef0967133f17a58626dc7248d15b5d8d29
Jans3n <jans3nsu@gmail.com>
2021-06-05 18:00:11 +0200

    Gebruiken controller
    instanties aangemaakt

--- src/main/java/controllers/SceneController.java
+++ src/main/java/controllers/SceneController.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,8 +16,13 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    LoginController loginController = new LoginController();
+
+    @FXML
+    TextField usernameField;
 
     public void switchToLobby(ActionEvent event) throws IOException {
+
         root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/Pre-Lobby.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -23,6 +30,8 @@ public class SceneController {
         stage.show();
     }
     public void switchToCreatedLobby(ActionEvent event) throws IOException {
+        loginController.testMessage(usernameField.getText());
+        loginController.checkCreate(usernameField.getText());
         root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/Lobby.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
