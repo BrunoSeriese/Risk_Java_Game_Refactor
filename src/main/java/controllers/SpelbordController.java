a18593a8dabd7d250cf9b068b954834c00d1404d
BrunoSeriese <bruno.pleysier@gmail.com>
2021-06-06 21:22:50 +0200

    SpelbordController met instructies

---
+++ src/main/java/controllers/SpelbordController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+public class SpelbordController {
+
+
+    public static void main(String[] args) {
+
+
+        // start spel hier met oproepen van een functie
+
+        // maak daarbij een Spelers ArrayList aan met al hun kaarten etc
+        // bij het aanmaken roept die een functie aan die de spelers kaarten geeft en andere dingen fikst
+
+        //Spelers players = new Spelers(args)
+
+        // maak een turn while loop
+
+        // laat bij winconditie de while loop aflopen
+
+
+        // print winnaar en laat zien op scherm
+    }
+}
