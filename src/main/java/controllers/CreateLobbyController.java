b7a26d9a4796e727065e2d668377221188c572f6
BrunoSeriese <bruno.pleysier@gmail.com>
2021-06-07 13:26:56 +0200

    scenecontroller opslitsing

--- src/main/java/controllers/CreateLobbyController.java
+++ src/main/java/controllers/CreateLobbyController.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CreateLobbyController {
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+
+    public void startGame(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/GameMap.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
