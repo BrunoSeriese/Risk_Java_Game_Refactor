d9858979fefb8ca2a569696ec9adbc81da57c6c4
Jans3n <jans3nsu@gmail.com>
2021-06-11 14:57:26 +0200

    Revert "aanvallende speler word uit het locale spel gepakt, rollDIce werkt nu voor aanvallende speler alles gecomment"
    This reverts commit 157525bd84d6483f50f6282afd88473085fef9ea.

--- src/main/java/controllers/SpelbordController.java
+++ src/main/java/controllers/SpelbordController.java
@@ -1,88 +1,73 @@
 package controllers;
-import application.State;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.WriteResult;
 import com.google.common.collect.Lists;
 import io.grpc.internal.JsonUtil;
-import models.DiceModel;
 import models.PlayerModel;
 
 import java.awt.*;
 import java.util.*;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class SpelbordController {
-//    PlayerModel playerModel;
-//    LoginController loginController = new LoginController();
-//
-//    public void showCards(){
-//    }
-//    public void showPlayers(){}
-//
-//
+
+
+    public void showCards(){
+    }
+    public void showPlayers(){}
+    public void rollDice(){
+
+
+    }
+
 //    private List<PlayerModel> spelers = new ArrayList<>();
 //
-//    public ArrayList getDefender(String lobbycode) throws ExecutionException, InterruptedException {
-//        DocumentReference docRef = State.database.getFirestoreDatabase().collection(lobbycode).document("players");
-//        ApiFuture<DocumentSnapshot> future = docRef.get();
-//        DocumentSnapshot document = future.get();
-//
-//        return (ArrayList) document.getData().get("players");
-//    }
-//
-//    public void rollDice() {
-//        playerModel = loginController.getInstance();
-//
-//        ArrayList<Integer> worp1 = new DiceModel().roll(3);
-//        ArrayList<Integer> worp2 = new DiceModel().roll(3);
-//        playerModel.setLastThrow(worp1);
-//
-//        ///TODO: ipv speler 1 aanmaken moet de speler die aanvalt uit de database worden gehaald en ipv speler 2 de verdediger
-//        /// Playermodel speler1= Speler uit database
-//
-//        System.out.println(playerModel);
-//
+////
+////    public void onClick() {
+////
+////        ArrayList<Integer> worp1 = new DiceController().roll(3);
+////        ArrayList<Integer> worp2 = new DiceController().roll(3);
+////
+////        PlayerModel speler1 = new PlayerModel("Petra", worp1); ///TODO: ipv speler 1 aanmaken moet de speler die aanvalt uit de database worden gehaald en ipv speler 2 de verdediger
+////        PlayerModel speler2 = new PlayerModel("Erik", worp2);  /// Playermodel speler1= Speler uit database
+////
+////
 ////        spelers.add(speler1);
 ////        spelers.add(speler2);
-//
-//    }
-//    public void aanval() {
 ////
-//        System.out.println("worp speler 1: "+ spelers.get(0).getLastThrow());
-//        System.out.println("worp speler 2: "+ spelers.get(1).getLastThrow());
+////    }
+////    public void aanval() {
+//////
+//////        System.out.println("worp speler 1: "+ spelers.get(0).getLastThrow());
+//////        System.out.println("worp speler 2: "+ spelers.get(1).getLastThrow());
+//////
+////        // (1) spelers gooien dobbelstenen
+//////        int attackThrow1 = spelers.get(0).getLastThrow().get(0);
+//////        int defendThrow1 = spelers.get(1).getLastThrow().get(0);
+//////        int attackThrow2= spelers.get(0).getLastThrow().get(1);
+//////        int defendThrow2 = spelers.get(1).getLastThrow().get(1);
+////
+////
+////        // (2) er wordt een winnaar bepaald
+////        PlayerModel winnaar = null;
+////        if (attackThrow1 > defendThrow1 && attackThrow2 > defendThrow2){
+////            System.out.println("speler 1 wint");//hier iets van spelers.get(1).setSoldaten(soldaten-2)
+////            winnaar = spelers.get();
+////        } else if(defendThrow1 >= attackThrow1 && defendThrow2 >= attackThrow2 ) {
+////            System.out.println("2 wint");
+////            winnaar = spelers.get(1);
+////
+////        }else {
+////            System.out.println("gelijkspel allebij een pion weg");
+////            System.out.println("STOP");
+////            return;
+////        }
 //
-//        //(1) spelers gooien dobbelstenen
-//        int attackThrow1 = spelers.get(0).getLastThrow().get(0);
-//        int defendThrow1 = spelers.get(1).getLastThrow().get(0);
-//        int attackThrow2= spelers.get(0).getLastThrow().get(1);
-//        int defendThrow2 = spelers.get(1).getLastThrow().get(1);
+//             (3) er wordt een willekeurige kaart gekozen
 //
-//
-//        // (2) er wordt een winnaar bepaald
-//        PlayerModel winnaar = null;
-//        if (attackThrow1 > defendThrow1 && attackThrow2 > defendThrow2){
-//            System.out.println("speler 1 wint");//hier iets van spelers.get(1).setSoldaten(soldaten-2)
-//            winnaar = spelers.get(0);
-//        } else if(defendThrow1 >= attackThrow1 && defendThrow2 >= attackThrow2 ) {
-//            System.out.println("2 wint");
-//            winnaar = spelers.get(1);
-//
-//        }else {
-//            System.out.println("gelijkspel allebij een pion weg");
-//            System.out.println("STOP");
-//            return;
-//        }
-//
-//        //(3) er wordt een willekeurige kaart gekozen
-//
-//        Integer willekeurigeKaart = new Random().nextInt(3)+1;
-//
+//          Integer willekeurigeKaart = new Random().nextInt(3)+1;
+
 ////            (4) de winnaar krijgt de willekerugei kaart
-//        winnaar.getCards().add(willekeurigeKaart);
-//
+////        winnaar.getCards().add(willekeurigeKaart);
+
 //
 //
 //
@@ -97,17 +82,17 @@ public class SpelbordController {
 ////
 //        // soldaten toevoegen
 //
-//    }
+//   }
 //    public void startGame(){
 //        spelers.get(0).setAantalLegers(20);
 //        spelers.get(0).setColor(Color.blue);
 //
 //    }
-//    public void removeCard (int cardNumber){
-//        spelers.get(0).getCards().remove(new Integer(cardNumber));
-//        spelers.get(0).getCards().remove(new Integer(cardNumber));
-//        spelers.get(0).getCards().remove(new Integer(cardNumber));
-//    }
+//public void removeCard (int cardNumber){
+//    spelers.get(0).getCards().remove(new Integer(cardNumber));
+//    spelers.get(0).getCards().remove(new Integer(cardNumber));
+//    spelers.get(0).getCards().remove(new Integer(cardNumber));
+//}
 //
 //    public void handInCard(){
 //        int Paarden= Collections.frequency(spelers.get(0).getCards(), 2);
@@ -135,46 +120,40 @@ public class SpelbordController {
 //        }
 //    }
 //
-//    //    public void showPlayers() {
-////    } //method voor de buttons in de UI/Interface - show players is een simpele popup met de namen van de players en kleur
-////
-////    public void rollDice() {
-////    } //method voor de buttons in de UI/Interface - roll dice daar moet je met martin ff overleggen
-////
-////    public void showCards() {
-////    } //method voor de buttons in de UI/Interface - show cards wordt een kleine nieuwe interface waar chiel en ryan aan gaan werken
-////
-//    public void cardToBase() throws ExecutionException, InterruptedException {
-//        DocumentReference docRef = State.database.getFirestoreDatabase().collection(State.lobbycode).document("players");
-//        ApiFuture<WriteResult> future = docRef.update("cards", spelers.get(0).getCards());
+//    public void showPlayers() {
+//    } //method voor de buttons in de UI/Interface - show players is een simpele popup met de namen van de players en kleur
 //
-//        WriteResult result = future.get();
-//        System.out.println("Write result: " + result);
-//    }
+//    public void rollDice() {
+//    } //method voor de buttons in de UI/Interface - roll dice daar moet je met martin ff overleggen
 //
-//    //    IK WEET BTW NIET OF DIT IN DE CONTROLLER MOET OF IN DE MODEL!!!
+//    public void showCards() {
+//    } //method voor de buttons in de UI/Interface - show cards wordt een kleine nieuwe interface waar chiel en ryan aan gaan werken
 //
-//
-//
-//    //maak aantal spelers gelijk aan hoeveel mensen in lobby, dus 4 nieuwe spelers.
-//    // de usernames kan je met .getUserName fzo pakken, kijk ff in de rest van de classes van jansen.
-//
-//    // de code kiest een random kleur en assigned die tot de speler OFFFFFF we kunnen 4 standaard kleuren kiezen bijvoorbeeld, rood blauw groen oranje.
-//
-//
-//    //als het spel is gestart heeft elke player een kleur en speler attributes, zoals regions cards etc...
-//    //ik maak nog een knopje met END TURN, dan als je erop klikt dat de beurt dan overgaat naar de volgende,
-//
-//
-//    // deze knop staat in de map Images en die mag alleen visible worden als degene heeft aangevallen/reinforced. maar dat komt later wel
-//
-//
-//    //if player has all regions, set hasWon = true, stop de turn loop >>> go to results screen
-//
-//
-//    //als een speler die 3 cards heeft dan schakelt de canExchageCards naar = true en dan verschijnt er een knopje die we nog moeten maken en die hele interface nog met, 'Trade in' bijvoorbeeld
-//    //dan verschijnt er een scherm met het aantal troepen dat diegene krijgt en dan klikt ie op "OK" en dan ontvangt hij de troepen en gaan die kaarten weg.
-//
-//
-//
+
+
+    //    IK WEET BTW NIET OF DIT IN DE CONTROLLER MOET OF IN DE MODEL!!!
+
+
+
+    //maak aantal spelers gelijk aan hoeveel mensen in lobby, dus 4 nieuwe spelers.
+    // de usernames kan je met .getUserName fzo pakken, kijk ff in de rest van de classes van jansen.
+
+    // de code kiest een random kleur en assigned die tot de speler OFFFFFF we kunnen 4 standaard kleuren kiezen bijvoorbeeld, rood blauw groen oranje.
+
+
+    //als het spel is gestart heeft elke player een kleur en speler attributes, zoals regions cards etc...
+    //ik maak nog een knopje met END TURN, dan als je erop klikt dat de beurt dan overgaat naar de volgende,
+
+
+    // deze knop staat in de map Images en die mag alleen visible worden als degene heeft aangevallen/reinforced. maar dat komt later wel
+
+
+    //if player has all regions, set hasWon = true, stop de turn loop >>> go to results screen
+
+
+    //als een speler die 3 cards heeft dan schakelt de canExchageCards naar = true en dan verschijnt er een knopje die we nog moeten maken en die hele interface nog met, 'Trade in' bijvoorbeeld
+    //dan verschijnt er een scherm met het aantal troepen dat diegene krijgt en dan klikt ie op "OK" en dan ontvangt hij de troepen en gaan die kaarten weg.
+
+
+
 }
--- src/main/java/models/PlayerModel.java
+++ src/main/java/models/PlayerModel.java
@@ -1,6 +1,5 @@
 package models;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 public class PlayerModel {
@@ -12,19 +11,19 @@ public class PlayerModel {
     private ArrayList<CountryModel> countries;
     private boolean canInterract;
 
-    private ArrayList<Integer> lastThrow;
+//    private ArrayList<Integer> lastThrow;
 
-    private ArrayList cards = new ArrayList();
-
-
-    private Color color; //random #,#,# digits for color code (ik weet niet of gewoon 'red' of 'blue' werkt, maar vgm moet het color codes, dus rgb(255,255,255) bijvoorbeeld
-    private Boolean hasWon; //set true if conquered all regions
-    private Boolean canExchangeCards; // if true, verschijnt button met trade in cards fzo, komt later
+//    private ArrayList cards = new ArrayList();
 
 
-    private int aantalLegers;
+//    private Color color; //random #,#,# digits for color code (ik weet niet of gewoon 'red' of 'blue' werkt, maar vgm moet het color codes, dus rgb(255,255,255) bijvoorbeeld
+//    private Boolean hasWon; //set true if conquered all regions
+//    private Boolean canExchangeCards; // if true, verschijnt button met trade in cards fzo, komt later
 
-    private int startLegers= 20;
+
+//    private int aantalLegers;
+
+//    private int startLegers;
 
 
     public PlayerModel(){
@@ -39,17 +38,13 @@ public class PlayerModel {
     }
 
     public PlayerModel(String username,  ArrayList<CountryModel> countries, ArrayList cards, Integer aantalLegers){
-        this.username = username;
-        this.countries = countries;
-        // is last throw echt nodig? last throw lijkt heel onnodig
-        this.lastThrow = lastThrow;
-        this.cards = cards;
-        this.aantalLegers=aantalLegers;
-        this.color=color;
-    }
-    public PlayerModel(String username, ArrayList lastThrow){
-        this.lastThrow=lastThrow;
-        this.username=username;
+//        this.username = username;
+//        this.countries = countries;
+//        // is last throw echt nodig? last throw lijkt heel onnodig
+//        this.lastThrow = lastThrow;
+//        this.cards = cards;
+//        this.aantalLegers=aantalLegers;
+//        this.color=color;
     }
 
 
@@ -105,44 +100,34 @@ public class PlayerModel {
         return countries;
     }
 
+    //
+//    public ArrayList<Integer> getLastThrow() {
+//        return lastThrow;
+//    }
+//
+//    public void setLastThrow(ArrayList<Integer> lastThrow) {
+//        this.lastThrow = lastThrow;
+//    }
 
-    public ArrayList<Integer> getLastThrow() {
-        return lastThrow;
-    }
+//    public int getAantalLegers() {
+//        return aantalLegers;
+//    }
+//
+//    public void setAantalLegers(int aantalLegers) {
+//        this.aantalLegers = aantalLegers;
+//    }
 
-    public ArrayList getCards() {
-        return cards;
-    }
-
-    public void setCards(ArrayList cards) {
-        this.cards = cards;
-    }
-
-    public void setLastThrow(ArrayList<Integer> lastThrow) {
-        this.lastThrow = lastThrow;
-    }
-
-    public int getAantalLegers() {
-        return aantalLegers;
-    }
-
-    public void setAantalLegers(int aantalLegers) {
-        this.aantalLegers = aantalLegers;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-    //Todo: om het te laten werken moet elke phase wachten op events van de kaart (klik events)
-    //Todo: klik events MOETEN TURNID ALTIJD VERGELIJKEN MET turnID van speler!
-    // door deze regeling kan de speler altijd
+//    public Color getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(Color color) {
+//        this.color = color;
+//    }
+        //Todo: om het te laten werken moet elke phase wachten op events van de kaart (klik events)
+        //Todo: klik events MOETEN TURNID ALTIJD VERGELIJKEN MET turnID van speler!
+        // door deze regeling kan de speler altijd
     // Todo hoe zorgen ervoor dat andere spelers in deze phase wel gewoon hun kaarten kunnen bekijken?
-
-
     public void buildPhase(){}
     public void attackPhase(){}
     public void endPhase(){}
