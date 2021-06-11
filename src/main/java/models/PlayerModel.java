349f32beeb8ccd28d362902a3f17c24565810152
MartinVeltman <martinveltman@live.nl>
2021-06-11 13:48:20 +0200

    aanvallende speler word uit het locale spel gepakt, rollDIce werkt nu voor aanvallende speler nog bezig om dit ook met de verdediger te doen alleen moete we eerst weten hoe we die willen definen

--- src/main/java/models/PlayerModel.java
+++ src/main/java/models/PlayerModel.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class PlayerModel {
@@ -11,19 +12,19 @@ public class PlayerModel {
     private ArrayList<CountryModel> countries;
     private boolean canInterract;
 
-//    private ArrayList<Integer> lastThrow;
+    private ArrayList<Integer> lastThrow;
 
-//    private ArrayList cards = new ArrayList();
-
-
-//    private Color color; //random #,#,# digits for color code (ik weet niet of gewoon 'red' of 'blue' werkt, maar vgm moet het color codes, dus rgb(255,255,255) bijvoorbeeld
-//    private Boolean hasWon; //set true if conquered all regions
-//    private Boolean canExchangeCards; // if true, verschijnt button met trade in cards fzo, komt later
+    private ArrayList cards = new ArrayList();
 
 
-//    private int aantalLegers;
+    private Color color; //random #,#,# digits for color code (ik weet niet of gewoon 'red' of 'blue' werkt, maar vgm moet het color codes, dus rgb(255,255,255) bijvoorbeeld
+    private Boolean hasWon; //set true if conquered all regions
+    private Boolean canExchangeCards; // if true, verschijnt button met trade in cards fzo, komt later
 
-//    private int startLegers;
+
+    private int aantalLegers;
+
+    private int startLegers= 20;
 
 
     public PlayerModel(){
@@ -38,13 +39,17 @@ public class PlayerModel {
     }
 
     public PlayerModel(String username,  ArrayList<CountryModel> countries, ArrayList cards, Integer aantalLegers){
-//        this.username = username;
-//        this.countries = countries;
-//        // is last throw echt nodig? last throw lijkt heel onnodig
-//        this.lastThrow = lastThrow;
-//        this.cards = cards;
-//        this.aantalLegers=aantalLegers;
-//        this.color=color;
+        this.username = username;
+        this.countries = countries;
+        // is last throw echt nodig? last throw lijkt heel onnodig
+        this.lastThrow = lastThrow;
+        this.cards = cards;
+        this.aantalLegers=aantalLegers;
+        this.color=color;
+    }
+    public PlayerModel(String username, ArrayList lastThrow){
+        this.lastThrow=lastThrow;
+        this.username=username;
     }
 
 
@@ -100,34 +105,44 @@ public class PlayerModel {
         return countries;
     }
 
-    //
-//    public ArrayList<Integer> getLastThrow() {
-//        return lastThrow;
-//    }
-//
-//    public void setLastThrow(ArrayList<Integer> lastThrow) {
-//        this.lastThrow = lastThrow;
-//    }
 
-//    public int getAantalLegers() {
-//        return aantalLegers;
-//    }
-//
-//    public void setAantalLegers(int aantalLegers) {
-//        this.aantalLegers = aantalLegers;
-//    }
+    public ArrayList<Integer> getLastThrow() {
+        return lastThrow;
+    }
 
-//    public Color getColor() {
-//        return color;
-//    }
-//
-//    public void setColor(Color color) {
-//        this.color = color;
-//    }
-        //Todo: om het te laten werken moet elke phase wachten op events van de kaart (klik events)
-        //Todo: klik events MOETEN TURNID ALTIJD VERGELIJKEN MET turnID van speler!
-        // door deze regeling kan de speler altijd
+    public ArrayList getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList cards) {
+        this.cards = cards;
+    }
+
+    public void setLastThrow(ArrayList<Integer> lastThrow) {
+        this.lastThrow = lastThrow;
+    }
+
+    public int getAantalLegers() {
+        return aantalLegers;
+    }
+
+    public void setAantalLegers(int aantalLegers) {
+        this.aantalLegers = aantalLegers;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    //Todo: om het te laten werken moet elke phase wachten op events van de kaart (klik events)
+    //Todo: klik events MOETEN TURNID ALTIJD VERGELIJKEN MET turnID van speler!
+    // door deze regeling kan de speler altijd
     // Todo hoe zorgen ervoor dat andere spelers in deze phase wel gewoon hun kaarten kunnen bekijken?
+
+
     public void buildPhase(){}
     public void attackPhase(){}
     public void endPhase(){}
