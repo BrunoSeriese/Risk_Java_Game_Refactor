35e8fea23407eca9478d37a4a9714d5708ecd851
Shallow-RB <ryan.bhola2601@gmail.com>
2021-06-11 02:13:49 +0200

    beginnetje aan lobbyview

--- src/main/java/views/LobbyView.java
+++ src/main/java/views/LobbyView.java
@@ -1,9 +1,53 @@
 package views;
 
-public class LobbyView {
+import application.State;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import controllers.LoginController;
+import javafx.fxml.FXML;
 
-//TODO zorg ervoor dat lobbycode en namen worden displayed met een setText(firestore->lobbycode)
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class LobbyView {
+    LoginController loginController = new LoginController();
+
+
+
+    //TODO zorg ervoor dat lobbycode en namen worden displayed met een setText(firestore->lobbycode)
+    @FXML
+    public void displayLobbyCode() {
+    }
+
+
 //TODO van firebase usernames pakken en die displayen in de lobby met een setText(firestore->username))
+  //TODO is nog niet af
+    public void getFirebaseUsernames(String lobbyCode) throws ExecutionException, InterruptedException {
+        //get benodigde stuff van firestore
+        DocumentReference docRef = State.database.getFirestoreDatabase().collection(lobbyCode).document("players");
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+        DocumentSnapshot document = future.get();
+
+        if (document.exists()) {
+
+            ArrayList<HashMap> arrayPlayerData = (ArrayList<HashMap>) document.get("players"); //zet alle data van 'players' in array wat hashmaps bevatten
+
+            for (HashMap playerData : arrayPlayerData) {
+                System.out.println(playerData);  //loopt door de arrays van firestore zodat je ze apart kan zien van elke player
+
+
+            }
+        } else {
+            System.out.println("niks");
+        }
+    }
+
+
+}
+
+
 //
 //   Stage lobbyStage;
 //
@@ -20,4 +64,4 @@ public class LobbyView {
 //       lobbyStage.setResizable(false);
 //       lobbyStage.show();
 //   }
-}
+
